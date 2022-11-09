d8abd63bcdb3bd9f010a6afb9210f7f6645716e7
Finn <finn.wu@higgstar.com>
2022-11-09 16:58:25 +0800

    feat: Update dependencies

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FinnM0reSPM",
-            dependencies: []),
+            dependencies: [
+                "SnapKit"
+            ]),
         .testTarget(
             name: "FinnM0reSPMTests",
             dependencies: ["FinnM0reSPM"]),
