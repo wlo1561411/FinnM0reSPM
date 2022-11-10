2038c5ca06556051667468226b1b5d35f4d0cec1
Finn <finn.wu@higgstar.com>
2022-11-10 16:21:39 +0800

    fix: UIButton styler

--- Sources/FinnM0reSPM/Styler/UIButton+Styler.swift
+++ Sources/FinnM0reSPM/Styler/UIButton+Styler.swift
@@ -1,14 +1,14 @@
 import UIKit
 
-public extension Styler where Base: UIButton {
+extension Styler where Base: UIButton {
     @discardableResult
-    func font(_ font: UIFont) -> Self {
+    public func font(_ font: UIFont) -> Self {
         base.titleLabel?.font = font
         return self
     }
     
     @discardableResult
-    func textColor(_ controlState: UIControl.State, _ color: UIColor) -> Self {
+    public func textColor(_ controlState: UIControl.State, _ color: UIColor) -> Self {
         base.setTitleColor(color, for: controlState)
         return self
     }
--- Sources/FinnM0reSPM/Styler/UIView+Styler.swift
+++ Sources/FinnM0reSPM/Styler/UIView+Styler.swift
@@ -2,27 +2,27 @@ import UIKit
 
 extension UIView: StylerCompatible { }
 
-public extension Styler where Base: UIView {
+extension Styler where Base: UIView {
     @discardableResult
-    func backgroundColor(_ color: UIColor) -> Self {
+    public func backgroundColor(_ color: UIColor) -> Self {
         base.backgroundColor = color
         return self
     }
     
     @discardableResult
-    func borderColor(_ color: UIColor) -> Self {
+    public func borderColor(_ color: UIColor) -> Self {
         base.layer.borderColor = color.cgColor
         return self
     }
     
     @discardableResult
-    func borderWidth(_ width: CGFloat) -> Self {
+    public func borderWidth(_ width: CGFloat) -> Self {
         base.layer.borderWidth = width
         return self
     }
     
     @discardableResult
-    func cornerRadius(_ radius: CGFloat) -> Self {
+    public func cornerRadius(_ radius: CGFloat) -> Self {
         base.clipsToBounds = true
         base.layer.cornerRadius = radius
         return self
