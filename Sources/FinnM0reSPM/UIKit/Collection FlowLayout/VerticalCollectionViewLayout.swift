1b6cfe3af498d44a70c175c7856dfba9e9f44ee0
Finn Wu (XN-SBK) <>
2023-08-02 14:20:24 +0800

    feat: VerticalCollectionViewLayout

--- Sources/FinnM0reSPM/UIKit/Collection FlowLayout/VerticalCollectionViewLayout.swift
+++ Sources/FinnM0reSPM/UIKit/Collection FlowLayout/VerticalCollectionViewLayout.swift
@@ -1,28 +1,15 @@
 import UIKit
 
-#warning("Section header footer is not include")
 @available(iOS 13.0, *)
+// Only support one section
 public class VerticalCollectionViewLayout: UICollectionViewFlowLayout {
   private var numberOfItemsPerColumn = 2
   private var spacing: CGFloat = 0
 
+  private var contentSize = CGSize.zero
+  private var itemAttributes = [IndexPath: UICollectionViewLayoutAttributes]()
+
   private var itemLayoutSizeFactory: (IndexPath) -> NSCollectionLayoutSize
-
-  private var itemsCount: Int {
-    collectionView?.numberOfItems(inSection: 0) ?? 0
-  }
-
-  private func itemWidth(at index: IndexPath) -> CGFloat {
-    getLayoutValue(
-      itemLayoutSizeFactory(index).widthDimension,
-      parentViewSize: collectionView?.frame.size ?? .zero)
-  }
-
-  private func itemHeight(at index: IndexPath) -> CGFloat {
-    getLayoutValue(
-      itemLayoutSizeFactory(index).heightDimension,
-      parentViewSize: collectionView?.frame.size ?? .zero)
-  }
 
   public init(
     numberOfItemsPerColumn: Int,
@@ -50,64 +37,26 @@ public class VerticalCollectionViewLayout: UICollectionViewFlowLayout {
     fatalError("init(coder:) has not been implemented")
   }
 
+  override public func prepare() {
+    itemAttributes.removeAll()
+    contentSize = .zero
+
+    super.prepare()
+
+    contentSize = calculateContentSize()
+  }
+
   override public var collectionViewContentSize: CGSize {
-    var totalWidth: CGFloat = 0
-    var totalHeight: CGFloat = 0
-
-    let numberOfSections = collectionView?.numberOfSections ?? 0
-
-    for section in 0..<numberOfSections {
-      let numberOfRows =
-        ceil(CGFloat(collectionView?.numberOfItems(inSection: section) ?? 0) / CGFloat(numberOfItemsPerColumn))
-
-      var rowWidths: [CGFloat] = Array(repeating: 0, count: Int(numberOfRows))
-
-      for row in 0..<Int(numberOfRows) {
-        for column in 0..<numberOfItemsPerColumn {
-          let item = row * numberOfItemsPerColumn + column
-
-          if item < collectionView?.numberOfItems(inSection: section) ?? 0 {
-            let indexPath = IndexPath(item: item, section: section)
-            let width = itemWidth(at: indexPath)
-            rowWidths[row] += width + spacing
-          }
-        }
-      }
-
-      // Subtract the spacing of the last item
-      totalWidth = max(totalWidth, rowWidths.reduce(0, +) - spacing)
-
-      var maxHeightInColumn: CGFloat = 0
-
-      for column in 0..<numberOfItemsPerColumn {
-        let indexPath = IndexPath(item: column, section: section)
-        let height = itemHeight(at: indexPath)
-
-        maxHeightInColumn = max(maxHeightInColumn, height)
-      }
-
-      // Subtract the spacing of the last item
-      totalHeight += CGFloat(numberOfItemsPerColumn) * (maxHeightInColumn + spacing) - spacing
-    }
-
-    return CGSize(width: totalWidth, height: totalHeight)
+    contentSize
   }
 
   override public func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-    let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+    guard itemAttributes[indexPath] == nil
+    else {
+      return itemAttributes[indexPath]
+    }
 
-    let column = indexPath.row % numberOfItemsPerColumn
-    let row = indexPath.row / numberOfItemsPerColumn
-
-    let itemWidth = itemWidth(at: indexPath)
-    let itemHeight = itemHeight(at: indexPath)
-
-    let x = CGFloat(row) * (itemWidth + spacing)
-    let y = CGFloat(column) * (itemHeight + spacing)
-
-    attributes.frame = CGRect(x: x, y: y, width: itemWidth, height: itemHeight)
-
-    return attributes
+    return getItemAttributes(at: indexPath)
   }
 
   override public func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
@@ -125,8 +74,66 @@ public class VerticalCollectionViewLayout: UICollectionViewFlowLayout {
 
       $0.frame = newAttributes.frame
     }
-      
+
     return attributesCopy
+  }
+}
+
+// MARK: - Data Handle
+
+@available(iOS 13.0, *)
+extension VerticalCollectionViewLayout {
+  private var itemsCount: Int {
+    collectionView?.numberOfItems(inSection: 0) ?? 0
+  }
+
+  private func itemWidth(at index: IndexPath) -> CGFloat {
+    getLayoutValue(
+      itemLayoutSizeFactory(index).widthDimension,
+      parentViewSize: collectionView?.frame.size ?? .zero)
+  }
+
+  private func itemHeight(at index: IndexPath) -> CGFloat {
+    getLayoutValue(
+      itemLayoutSizeFactory(index).heightDimension,
+      parentViewSize: collectionView?.frame.size ?? .zero)
+  }
+
+  private func getItemAttributes(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes {
+    let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+
+    let column = indexPath.row % numberOfItemsPerColumn
+    let row = indexPath.row / numberOfItemsPerColumn
+
+    let itemWidth = itemWidth(at: indexPath)
+    let itemHeight = itemHeight(at: indexPath)
+
+    let x = CGFloat(row) * (itemWidth + spacing)
+    let y = CGFloat(column) * (itemHeight + spacing)
+
+    attributes.frame = CGRect(x: x, y: y, width: itemWidth, height: itemHeight)
+
+    itemAttributes[indexPath] = attributes
+
+    return attributes
+  }
+
+  private func calculateContentSize() -> CGSize {
+      let rows = ceil(CGFloat(itemsCount) / CGFloat(numberOfItemsPerColumn))
+      let totalWidth = (0 ..< Int(rows))
+          .map {
+              itemWidth(at: [0, $0]) + spacing
+          }
+          .reduce(0, +) - spacing
+
+      let columns = numberOfItemsPerColumn
+      let totalHeight = (0 ..< columns)
+          .map {
+              itemHeight(at: [0, $0]) + spacing
+          }
+          .reduce(0, +) - spacing
+      
+      return CGSize(width: totalWidth, height: totalHeight)
   }
 
   private func getLayoutValue(_ layout: NSCollectionLayoutDimension, parentViewSize: CGSize) -> CGFloat {
